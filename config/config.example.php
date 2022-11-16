6cae0983121a85d8be89a8f45b175edab00d1ec1
AliasPedroKarim <karimisma6464@gmail.com>
2022-11-16 02:44:28 +0100

    config example

---
+++ config/config.example.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    "db_user" => "nawastia_db",
+    "db_pass" => "PASSWORD_HARD",
+    "db_host" => "localhost",
+    "db_name" => "nawastia_db",
+    "port" => 3306,
+    "charset_default" => "utf8mb4"
+);
